8e561586ee8f3a38b0a0d91c409645a7d1c0a1c8
Meep <79652955+switchmeep@users.noreply.github.com>
2022-01-07 23:20:50 -0600

    testing

--- kahootSmasher.py
+++ kahootSmasher.py
@@ -15,47 +15,63 @@ Thank you for using kahootSmash.
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import random, string, time, ctypes
+import random, string, time, ctypes, threading
 
 ctypes.windll.kernel32.SetConsoleTitleA("kahootSmasher")
 chrome_options = Options()
-chrome_options.add_argument("--incognito --disable-logging --log-level=3")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--log-level=3")
 
 PATH = '.\chromedriver.exe'
-driver = webdriver.Chrome(executable_path = PATH, chrome_options=chrome_options)
 kahootUrl = 'https://kahoot.it/'
 tabNum = 0
+threadCount = 0
 
-pin = input("Game Pin:\n")
+class Smasher:
+    def __init__(self, pin=None):
+        self.pin = pin
+        self.drivers = []
 
-def randomString():
-    letters = string.ascii_lowercase
-    outputString = ''.join(random.choice(letters) for i in range(10))
-    return outputString
+    def randomString(self):
+        letters = string.ascii_lowercase
+        outputString = ''.join(random.choice(letters) for i in range(10))
+        return outputString
 
-def createBot(gamePin):
-    global tabNum, driver, kahootUrl
-    driver.get(kahootUrl)
-    inputBox = driver.find_element_by_id('game-input')
-    inputBox.click()
-    inputBox.send_keys(gamePin)
-    inputBox.send_keys(Keys.RETURN)
+    def createBot(self):
+        global tabNum
+        bot = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
+        self.drivers.append(bot)
 
-    try:
-        time.sleep(2)
-        randomUser = randomString()
-        nicknameBox = driver.find_element_by_id('nickname')
-        nicknameBox.click()
-        nicknameBox.send_keys(randomUser)
-        nicknameBox.send_keys(Keys.RETURN)
-    except:
-        print("Invalid Game Pin!")
-        exit()
+        while True:
+            bot.get(kahootUrl)
+            inputBox = bot.find_element_by_id('game-input')
+            inputBox.click()
+            inputBox.send_keys(self.pin)
+            inputBox.send_keys(Keys.RETURN)
 
-    time.sleep(3)
-    driver.execute_script("window.open('');")
-    tabNum = tabNum + 1
-    driver.switch_to.window(driver.window_handles[tabNum])
+            try:
+                time.sleep(2)
+                randomUser = self.randomString()
+                nicknameBox = bot.find_element_by_id('nickname')
+                nicknameBox.click()
+                nicknameBox.send_keys(randomUser)
+                nicknameBox.send_keys(Keys.RETURN)
+            except:
+                print("Invalid Game Pin!")
+                exit()
+
+            time.sleep(3)
+            bot.execute_script("window.open('');")
+            tabNum = tabNum + 1
+            bot.switch_to.window(bot.window_handles[tabNum])
     
-while True:
-    createBot(pin)
+if __name__ == "__main__":
+    print(range(2))
+    smash = Smasher(input("Game Pin: "))
+    threadCount = int(input("Thread Count: "))
+    print(f'Flooding with {threadCount} threads.')
+    
+    for x in range(threadCount):
+        print("started thread")
+        t = threading.Thread(target=smash.createBot())
+        t.start()
