eac449c39a27ae551bc568c2a8633e53abb2dbc9
Meep <79652955+switchmeep@users.noreply.github.com>
2022-01-09 15:18:31 -0600

    released 1.2

--- rpc.py
+++ rpc.py
@@ -11,25 +11,25 @@ class RPC:
         large_image="kahooticon",
         details="Flooding Kahoots with Bots",
         large_text="Spams Kahoots with bot users",
-        buttons=[{"label":"Download Kahoot Smasher", "url":"https://google.com"}])
+        buttons=[{"label":"Download Kahoot Smasher", "url":"https://github.com/botmeep/kahootSmash/"}])
     def updatePin(self, pin):
         self.pin = pin
         self.rpc.update(details=f'Flooding Kahoot: {pin}',
         large_image="kahooticon",
         state="Flooding Kahoots with Bots",
         large_text="Spams Kahoots with bot users",
-        buttons=[{"label":"Download Kahoot Smasher", "url":"https://google.com"}],
+        buttons=[{"label":"Download Kahoot Smasher", "url":"https://github.com/botmeep/kahootSmash/"}],
         instance=True)
     def updateBotCount(self, botCount):
         self.rpc.update(details=f'Flooding Kahoot: {self.pin}',
         large_image="kahooticon",
         state=f'Flooding Kahoots with {botCount} Bots',
         large_text="Spams Kahoots with bot users",
-        buttons=[{"label":"Download Kahoot Smasher", "url":"https://google.com"}],
+        buttons=[{"label":"Download Kahoot Smasher", "url":"https://github.com/botmeep/kahootSmash/"}],
         instance=True)
     def clear(self):
         self.rpc.update(state="Currently Idle",
         large_image="kahooticon",
         details="Flooding Kahoots with Bots",
         large_text="Spams Kahoots with bot users",
-        buttons=[{"label":"Download Kahoot Smasher", "url":"https://google.com"}])
+        buttons=[{"label":"Download Kahoot Smasher", "url":"https://github.com/botmeep/kahootSmash/"}])
